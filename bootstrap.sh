3938d6ac191a9cd8b35d950f8aab65b509a5dbc7
Bjørn Jarle Kvande <bjorn.jarle.kvande@gmail.com>
2025-08-27 12:29:50 +0200

    implementing mac version - not quite done yet

--- bootstrap.sh
+++ bootstrap.sh
@@ -28,9 +28,39 @@ set -e
 source "$(dirname "$0")/core.sh"
 
 # Cleanup on exit
-trap unmountDriveOnCleanup EXIT
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  trap unmountDriveOnCleanup EXIT
+fi
+
 
 DOTFILES_DIR="$HOME/.dotfiles"
+
+installDeveloperEssentialsTools() {
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    sudo apt update
+    sudo apt install -y zsh tmux git curl build-essential direnv
+    sudo apt install -y wget gpg apt-transport-https software-properties-common
+  elif [[ "$OSTYPE" == darwin* ]]; then
+    # homebrew is the equivalent of apt on mac
+    echo "Installing homebrew..."
+    if ! command -v brew &>/dev/null; then
+      /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    else
+      echo "Homebrew already installed"
+    fi
+    if [[ -d "/opt/homebrew/bin" ]]; then
+      eval "$(/opt/homebrew/bin/brew shellenv)"
+    elif [[ -d "/usr/local/bin" ]]; then
+      eval "$(/usr/local/bin/brew shellenv)"
+    fi
+
+    echo "Updating homebrew..."
+    brew update
+
+    echo "Installing tmux git direnv wget gnupg..."
+    brew install tmux git direnv wget gnupg
+  fi
+}
 
 configureDotFiles() {
   # the files to copy into the directory and link from the home directory
@@ -68,6 +98,81 @@ configureDotFiles() {
   done
 }
 
+installVSCode() {
+  # install VS code
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    echo "Adding Microsoft repository for VS Code..."
+    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+    sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+    sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+    sudo apt update -y
+    echo "Installing VS Code..."
+    sudo apt install -y code
+  elif [[ "$OSTYPE" == darwin* ]]; then
+    if ! command -v code &>/dev/null; then
+      echo "Installing VS Code..."
+      brew install --cask visual-studio-code
+    else
+      echo "VS Code already installed"
+    fi
+  fi
+
+  # install extensions
+  if [ -f "vscode-extensions.txt" ]; then
+    echo "Installing VS Code extensions..."
+    # Clean the file: remove empty lines and trim spaces
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+      sed -i '/^$/d;s/^[ \t]*//;s/[ \t]*$//' vscode-extensions.txt
+    fi
+    # Install each extension
+    cat vscode-extensions.txt | xargs -n 1 code --install-extension
+  else
+    echo "No vscode-extensions.txt file found; skipping extensions."
+  fi
+
+  # set default configuration
+  echo "Setting default configuration for VS Code..."
+  cp "vscode_settings.json" "$DOTFILES_DIR"
+  VSCODE_USER_DIR="$HOME/.config/Code/User"
+  if [[ "$OSTYPE" == darwin* ]]; then
+    VSCODE_USER_DIR="$HOME/Library/Application Support/Code/User"
+  fi
+  mkdir -p "$VSCODE_USER_DIR"
+  if [ -e "$VSCODE_USER_DIR/settings.json" ] || [ -L "$VSCODE_USER_DIR/settings.json" ]; then
+    rm -f "$VSCODE_USER_DIR/settings.json"
+  fi
+  ln -s "$DOTFILES_DIR/vscode_settings.json" "$VSCODE_USER_DIR/settings.json"
+}
+
+configureSecrets() {
+  # mount our secret drive with the keys
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    mountSecretDrive
+  fi
+
+  # make sure we have our .ssh directory
+  if [ ! -d "$HOME/.ssh" ]; then
+    mkdir -p "$HOME/.ssh"
+    echo "Created $HOME/.ssh directory."
+  else
+    echo "$HOME/.ssh directory already exists."
+  fi
+
+  # configure our .ssh directory with the secrets from the USB stick
+  MOUNT_POINT="/media/veracrypt1"
+  if [[ "$OSTYPE" == darwin* ]]; then
+    MOUNT_POINT="/Volumes/BJORN_DEV_MAC"
+  fi
+
+  cp "$MOUNT_POINT"/secrets/ssh/* ~/.ssh/
+  echo "Copied SSH secrets to ~/.ssh/"
+  chmod 700 ~/.ssh
+  chmod 600 ~/.ssh/id_*
+  chmod 644 ~/.ssh/id_*.pub
+  chmod 644 ~/.ssh/known_hosts
+  chmod 644 ~/.ssh/config
+}
+
 installNode() {
   # nvm (node version manager) is used to install/use different versions of node
   if [ ! -d "$HOME/.nvm" ]; then
@@ -86,72 +191,9 @@ installNode() {
   nvm use default
 
   # make sure we can run trailguide on https 
-  sudo setcap 'cap_net_bind_service=+ep' $(which node)
-}
-
-installAndConfigureVSCode() {
-  # install VS Code
-  echo "Adding Microsoft repository for VS Code..."
-  wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-  sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
-  sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-  sudo apt update -y
-  echo "Installing VS Code..."
-  sudo apt install -y code
-
-  # Install VS Code extensions
-  if [ -f "vscode-extensions.txt" ]; then
-    echo "Installing VS Code extensions..."
-    # Clean the file: remove empty lines and trim spaces
-    sed -i '/^$/d;s/^[ \t]*//;s/[ \t]*$//' vscode-extensions.txt
-    # Install each extension
-    cat vscode-extensions.txt | xargs -n 1 code --install-extension
-  else
-    echo "No vscode-extensions.txt file found; skipping extensions."
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    sudo setcap 'cap_net_bind_service=+ep' "$(which node)"
   fi
-
-  # set our default VS Code settings
-  cp "vscode_settings.json" "$DOTFILES_DIR"
-
-  # symlink the default Visual Studio Code settings file to the copied file
-  echo "Setting default configuration for VS Code..."
-  VSCODE_USER_DIR="$HOME/.config/Code/User"
-  mkdir -p "$VSCODE_USER_DIR"
-  if [ -e "$VSCODE_USER_DIR/settings.json" ] || [ -L "$VSCODE_USER_DIR/settings.json" ]; then
-    rm -f "$VSCODE_USER_DIR/settings.json"
-  fi
-  ln -s "$DOTFILES_DIR/vscode_settings.json" "$VSCODE_USER_DIR/settings.json"
-}
-
-installAndStartMongoDB() {
-  wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
-  echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
-  sudo apt-get update
-  sudo apt-get install -y mongodb-org
-  sudo systemctl enable mongod
-  sudo systemctl start mongod
-}
-
-configureSecrets() {
-  # mount our secret drive and copy the keys into the .ssh directory
-  mountSecretDrive
-
-  # make sure we have our .ssh directory
-  if [ ! -d "$HOME/.ssh" ]; then
-    mkdir -p "$HOME/.ssh"
-    echo "Created $HOME/.ssh directory."
-  else
-    echo "$HOME/.ssh directory already exists."
-  fi
-
-  # configure our .ssh directory with the secrets from the USB stick
-  cp "$MOUNT_POINT"/secrets/ssh/* ~/.ssh/
-  echo "Copied SSH secrets to ~/.ssh/"
-  chmod 700 ~/.ssh
-  chmod 600 ~/.ssh/id_*
-  chmod 644 ~/.ssh/id_*.pub
-  chmod 644 ~/.ssh/known_hosts
-  chmod 644 ~/.ssh/config
 }
 
 checkoutProjects() {
@@ -215,153 +257,59 @@ checkoutProjects() {
   fi
 }
 
-bootstrapLinux() {
-  # install developer essentials
-  sudo apt update
-  sudo apt install -y zsh tmux git curl build-essential direnv
-  sudo apt install -y wget gpg apt-transport-https software-properties-common
-
-  echo "Install and configure Node..."
-  installNode
-
-  echo "Setting up home directory dot files..."
-  configureDotFiles
-
-  echo "Install and configure VS Code..."
-  installAndConfigureVSCode
-
-  # make zsh the default shell 
-  if [ "$SHELL" != "$(which zsh)" ]; then
-    echo "Changing default shell to zsh..."
-    chsh -s "$(which zsh)"
-  else
-    echo "Default shell is already zsh."
+installAndStartMongoDB() {
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+    echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+    sudo apt-get update
+    sudo apt-get install -y mongodb-org
+    sudo systemctl enable mongod
+    sudo systemctl start mongod
+  elif [[ "$OSTYPE" == darwin* ]]; then
+    echo "Installing MongoDB on Mac is not implemented yet..."
+    # # # Install MongoDB 4.4
+    # # echo "Installing MongoDB 4.4..."
+    # # brew install mongodb-community@4.4
   fi
-
-  echo "Install and start the Mongo DB..."
-  installAndStartMongoDB
-
-  echo "Set up our secrets..."
-  configureSecrets
-
-  echo "Checkout my projects..."
-  checkoutProjects
 }
 
+bootstrap() {
+  if [[ "$OSTYPE" == "linux-gnu" || "$OSTYPE" == "darwin"* ]]; then
+    echo "Installing developer essentials..."
+    installDeveloperEssentialsTools
 
-# bootstrap a mac machine
-bootstrapMac() {
-  echo "Bootstrapping macOS is not implemented yet..."
-  # # Install Homebrew if not already installed
-  # if ! command -v brew &>/dev/null; then
-  #   echo "Installing Homebrew..."
-  #   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  # else
-  #   echo "Homebrew already installed"
-  # fi
+    echo "Setting up home directory dot files..."
+    configureDotFiles
 
-  # # Add Homebrew to PATH for the current session
-  # if [[ -d "/opt/homebrew/bin" ]]; then
-  #   eval "$(/opt/homebrew/bin/brew shellenv)"
-  # elif [[ -d "/usr/local/bin" ]]; then
-  #   eval "$(/usr/local/bin/brew shellenv)"
-  # fi
+    echo "Install and configure VS Code..."
+    installVSCode
 
-  # echo "Updating Homebrew..."
-  # brew update
+    echo "Set up our secrets..."
+    configureSecrets
 
-  # # # Install developer essentials
-  # brew install tmux git direnv wget gnupg
+    echo "Install and configure Node..."
+    installNode
 
-  # # nvm (node version manager)
-  # echo "Installing Node Version Manager..."
-  # if [ ! -d "$HOME/.nvm" ]; then
-  #   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
-  # else
-  #   echo "Node Version Manager already installed"
-  # fi
-  # export NVM_DIR="$HOME/.nvm"
-  # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-  # [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+      echo "Checkout my projects..."
+      checkoutProjects
+    fi
 
-  # # wr use node version 16 as default
-  # nvm install 16
-  # nvm alias default 16
-  # nvm use default
+    echo "Install and start the Mongo DB..."
+    installAndStartMongoDB
 
-  # # Install VS Code
-  # if ! command -v code &>/dev/null; then
-  #   echo "Installing VS Code..."
-  #   brew install --cask visual-studio-code
-  # else
-  #   echo "VS Code already installed"
-  # fi
-
-  # # create symlinks for the .dot files 
-  # echo "Creating symbolic links for dotfiles..."
-  # DOTFILES_DIR="$HOME/.dotfiles"
-  # FILES_TO_LINK=(
-  #   ".gitconfig:$DOTFILES_DIR/.gitconfig"
-  #   ".zprofile:$DOTFILES_DIR/.zprofile"
-  #   ".zshrc:$DOTFILES_DIR/.zshrc"
-  #   ".tmux.conf:$DOTFILES_DIR/config/tmux.conf"
-  # )
-
-  # for entry in "${FILES_TO_LINK[@]}"; do
-  #   file="${entry%%:*}"     # left side before :
-  #   target="${entry##*:}"   # right side after :
-  #   link="$HOME/$file"
-  #   if [ -e "$link" ] || [ -L "$link" ]; then
-  #       echo "Backing up existing $link to $link.bak"
-  #       mv "$link" "$link.bak"
-  #   fi
-  #   echo "Creating symlink: $link -> $target"
-  #   ln -s "$target" "$link"
-  # done
-
-  # # # Install VS Code extensions
-  # if [ -f "vscode-extensions.txt" ]; then
-  #   echo "Installing VS Code extensions..."
-  #   cat vscode-extensions.txt | xargs -n 1 code --install-extension
-  # else
-  #   echo "No vscode-extensions.txt file found; skipping extensions."
-  # fi
-
-
-  # # # symlink the default Visual Studio Code settings file
-  # echo "Creating symbolic link for the default Visual Studio Code settings file..."
-  # VSCODE_USER_DIR="$HOME/Library/Application Support/Code/User"
-  # mkdir -p "$VSCODE_USER_DIR"
-  # if [ -L "$VSCODE_USER_DIR/settings.json" ]; then
-  #     mv "$VSCODE_USER_DIR/settings.json" "$VSCODE_USER_DIR/settings.json.bak"
-  # fi
-  # if [ -e "$VSCODE_USER_DIR/settings.json" ] || [ -L "$VSCODE_USER_DIR/settings.json" ]; then
-  #     rm -f "$VSCODE_USER_DIR/settings.json"
-  # fi
-  # ln -s "$DOTFILES_DIR/config/Code/User/settings.json" "$VSCODE_USER_DIR/settings.json"
-
-  # # # Install MongoDB 4.4
-  # # echo "Installing MongoDB 4.4..."
-  # # brew install mongodb-community@4.4
-
-  # # make sure our .ssh directory has correct permissions 
-  # echo "Setting permissions for .ssh directory..."
-  # chmod 700 ~/.ssh
-  # chmod 600 ~/.ssh/id_*
-  # chmod 644 ~/.ssh/id_*.pub
-  # chmod 644 ~/.ssh/known_hosts
-  # chmod 644 ~/.ssh/config
+    # make zsh the default shell 
+    if [ "$SHELL" != "$(which zsh)" ]; then
+      echo "Changing default shell to zsh..."
+      chsh -s "$(which zsh)"
+    else
+      echo "Default shell is already zsh."
+    fi
+  else
+    echo "Unsupported OS: $OSTYPE"
+    exit 1
+  fi
 }
 
 # try to bootstrap our new machine
-if [ "$OSTYPE" == "linux-gnu" ]; then
-  echo "Detected Linux"
-  bootstrapLinux
-elif [[ "$OSTYPE" == darwin* ]]; then
-  echo "Detected macOS"
-  bootstrapMac
-else
-  echo "Unsupported OS: $OSTYPE"
-  exit 1
-fi
-
+bootstrap
