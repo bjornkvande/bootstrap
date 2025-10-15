afdb0880644f4d30af67dc94ff55074feae62158
Bjørn Jarle Kvande <bjorn.jarle.kvande@gmail.com>
2025-10-15 16:24:27 +0200

    copying nginx config file

--- linode.sh
+++ linode.sh
@@ -6,8 +6,9 @@ echo "Updating system packages..."
 sudo apt update && sudo apt upgrade -y
 
 # Install nginx
-echo "Installing Nginx..."
+echo "Installing nginx..."
 sudo apt install -y nginx
+cp nginx.conf /etc/nginx/nginx.conf
 
 # Test and reload nginx
 sudo nginx -t
