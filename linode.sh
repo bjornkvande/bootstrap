0d5364be1cf565a01691278e984aae7d039dd650
Bjørn Jarle Kvande <bjorn.jarle.kvande@gmail.com>
2025-10-17 11:59:37 +0200

    added unzip for deno installation

--- linode.sh
+++ linode.sh
@@ -8,8 +8,12 @@ cp .bashrc_ubuntu ~/.bashrc
 echo "Updating system packages..."
 sudo apt update && sudo apt upgrade -y
 
+# Installing essential packages
+echo "Installing essential packages..."
+sudo apt install -y unzip
+
 # Install nginx
-echo "Installing nginx..."
+echo "Installing nginx and related packages..."
 sudo apt install -y nginx certbot python3-certbot-nginx
 
 # Make sure we have the correct access to the web root folder
